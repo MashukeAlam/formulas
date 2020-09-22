9f65ee0a4955e480bcfa54894587f50c7d10a5f3
MashukJim <jimmashuke@gmail.com>
2020-09-22 10:27:59 +0600

    Truth

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const C = 299792458;
 
+//formulas
 module.exports.massenergy = mass =>  mass * C * C;
+
+//convertions
+module.exports.acre2sqft = acre =>  acre * 43560;
+module.exports.acre2yd = acre =>  acre * 4840;
+module.exports.acre2sqmeter = acre =>  acre * 4047;
+module.exports.cm2inch = cm =>  cm * 0.394;
+module.exports.cm2feet = cm =>  cm * 0.0328;
+module.exports.cm2meter = cm =>  cm * 0.01;
+module.exports.cm2millimeter = cm =>  cm * 10;
+module.exports.cup2ounce = cup =>  cup * 8;
+module.exports.cup2tablespoons = cup =>  cup * 16;
+module.exports.feet2inch = ft =>  ft * 12;
+module.exports.feet2millimeter = ft =>  ft * 304.8;
+module.exports.feet2centimeter = ft =>  ft * 30.48;
+module.exports.feet2meter = ft =>  ft * 0.3048;
+module.exports.gallon2liter = g =>  g * 3.785;
+module.exports.grams2mg = g =>  g * 1000;
+module.exports.grams2kg = g =>  g * 0.001;
+module.exports.inch2mm = i =>  i * 25.4;
+module.exports.mileperhour2feetperhour = m =>  m * 88;
+module.exports.mileperhour2kiloperhour = m =>  m * 1609;
+
+
+
